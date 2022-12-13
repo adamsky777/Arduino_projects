3de7935519c3734884b4b914063d8784c02a08c5
Adam Torkos <73426467+adamsky777@users.noreply.github.com>
2022-12-13 16:48:40 +0100

    Add files via upload

---
+++ temp_monitor.ino
@@ -0,0 +1,20 @@
+float temp;
+int tempPin = 0;
+
+void setup() {
+   Serial.begin(9600);
+}
+
+void loop() {
+   temp = analogRead(tempPin);
+   // read analog volt from sensor and save to variable temp
+   temp = temp/1024*5;
+   temp = temp-0.5;
+   temp = temp/0.01;
+   // convert the analog volt to its temperature equivalent
+   Serial.print("TEMPERATURE = ");
+   Serial.print(temp); // display temperature value
+   Serial.print("*C");
+   Serial.println();
+   delay(2000); // update sensor reading each one second 
+}
