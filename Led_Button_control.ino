cf0d7dba79fd865d132dad501fd15e13ae9042fb
Adam Torkos <73426467+adamsky777@users.noreply.github.com>
2022-12-14 16:37:05 +0000

    Update Led_Button_control.ino

--- Led_Button_control.ino
+++ Led_Button_control.ino
@@ -3,6 +3,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(3, OUTPUT);
   pinMode(2 , INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -10,18 +11,18 @@ void loop() {
   switchState = digitalRead(2);
   if (switchState == LOW) {
     digitalWrite(3, HIGH);
+    Serial.print("LED ON");
+    Serial.println();
+    delay(100);
     }
 
   else {
-
     digitalWrite(3, LOW);
     delay(100);
     digitalWrite(3, HIGH);
     delay(100);
     digitalWrite(3, LOW);
-
+    Serial.print("LED Blinking");
+    Serial.println();
     }
-
-
-
 }
